4661dd6f9e61792f4a47d19785a0478dac63753d
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-10-30 11:34:24 -0300

    adicionando validacao de usuario e datas

--- src/main/java/com/eder/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/eder/todolist/filter/FilterTaskAuth.java
@@ -19,42 +19,49 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FilterTaskAuth extends OncePerRequestFilter {
 	@Autowired
 	private UserRepository userRepository;
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
-		var autorizacao = request.getHeader("Authorization");
-		System.out.println(autorizacao);
-		var user_pass = autorizacao.substring("Basic".length()).trim();
-		System.out.println(user_pass);
-		
-		byte[] authDecode = Base64.getDecoder().decode(user_pass);
-		var authString = new String(authDecode);
-		String [] credencial = authString.split(":");
-		String username =  credencial[0];
-		String password =  credencial[1];
-		System.out.println(username);
-		System.out.println(password);
-		
-		// pegar a autentcação(usuario e senha)
-		
-		var user = this.userRepository.findByUsername(username);
-		if(user==null) {
-			response.sendError(401,"Usuário sem autorização");
-		}else {
-			//validar senha
-			var verificarSenha = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
-			if(verificarSenha.verified) {
-				filterChain.doFilter(request, response);
-			}else {
-				response.sendError(401);
+		var servletPath = request.getServletPath();
+		if (servletPath.equals("/tasks/")) {
+			// pegar a autenticação(usuario e senha)
+
+			var autorizacao = request.getHeader("Authorization");
+			System.out.println(autorizacao);
+			var user_pass = autorizacao.substring("Basic".length()).trim();
+			System.out.println("sem os espaços");
+			System.out.println(user_pass);
+
+			byte[] authDecode = Base64.getDecoder().decode(user_pass);
+			System.out.println(authDecode);
+			var authString = new String(authDecode);
+			String[] credencial = authString.split(":");
+			String username = credencial[0];
+			String password = credencial[1];
+			System.out.println("Posição [0] do vetor: " + username);
+			System.out.println("Posição [1] do vetor:" + password);
+
+			// pegar a autentcação(usuario e senha)
+
+			var user = this.userRepository.findByUsername(username);
+			if (user == null) {
+				response.sendError(401, "Usuário sem autorização");
+			} else {
+				// validar senha
+				var verificarSenha = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+				if (verificarSenha.verified) {
+					request.setAttribute("idUser",user.getId());//pegando o id do usuario da requisição
+					filterChain.doFilter(request, response);
+				} else {
+					response.sendError(401);
+				}
 			}
+
+		} else {
+			// segue o fluxo
+			filterChain.doFilter(request, response);
 		}
-		
-		
-		//segue o fluxo
-     filterChain.doFilter(request, response);
-     
 
 	}
 
--- src/main/java/com/eder/todolist/task/TaskController.java
+++ src/main/java/com/eder/todolist/task/TaskController.java
@@ -1,10 +1,18 @@
 package com.eder.todolist.task;
 
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,8 +21,21 @@ public class TaskController {
 	private TaskRepository repository;
 
 	@PostMapping("/")
-	public TaskModel create(@RequestBody TaskModel taskModel) {
+	public ResponseEntity create(@RequestBody TaskModel taskModel,HttpServletRequest request) {
+		System.out.println("Chegou  no controller " + request.getAttribute("idUser"));
+		var idUser = request.getAttribute("idUser");
+		taskModel.setIdUser((UUID)idUser);
+		
+		var currentDate = LocalDateTime.now();
+		System.out.println(currentDate);
+		if(currentDate.isAfter(taskModel.getStartAt())|| currentDate.isAfter(taskModel.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio/termino deve ser maior "
+					+ "do que a data atual");
+		}
+		if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio deve ser menor do que a data de termino");
+		}
 		var task = this.repository.save(taskModel);
-		return  task;
+		return  ResponseEntity.status(HttpStatus.OK).body(task);
 	}
 }
